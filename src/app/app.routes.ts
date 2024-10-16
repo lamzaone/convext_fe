dbd0e7f4e60bf1bc1533bba3dff40e74bd7016da
Denis <denisdenis009@gmail.com>
2024-10-17 00:42:30 +0300

    Add homepage component, homepage routing, router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomepageComponent,
+
+  }
+];
