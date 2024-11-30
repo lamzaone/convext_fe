b0adad0c28653e816a7eb518019a519900e40b9f
Denis <denisdenis009@gmail.com>
2024-11-30 21:35:25 +0200

    Added dropdown menu for user related stuff

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Signal} from '@angular/core';
+import { Component, effect, signal} from '@angular/core';
 import { AuthServiceService } from '../services/auth-service.service';
 import { Router, RouterLinkActive, RouterLink} from '@angular/router';
 
@@ -11,8 +11,23 @@ import { Router, RouterLinkActive, RouterLink} from '@angular/router';
 })
 export class NavigationComponent {
   currentUser: any;
+  userDropdown = signal(false);
 
-  constructor(private authService: AuthServiceService) { }
+  constructor(private authService: AuthServiceService) {
+    // Effect to change the visibility of the dropdown on userDropdown signal change
+    // Tried a few other ways using .toggle but failed to get it to work so i guess this works
+    effect(() =>
+        {
+          if (this.userDropdown()){
+            document.querySelector('.dropdown')?.classList.remove('hidden');
+          }
+          else {
+            document.querySelector('.dropdown')?.classList.add('hidden')
+          };
+        }
+    );
+  }
+
   ngOnInit() {
     this.currentUser = this.authService.userData;
   }
@@ -24,6 +39,12 @@ export class NavigationComponent {
   loginWithGoogle(){
     this.authService.loginWithGoogle();
   }
+
+  toggleDropdown() {
+    this.userDropdown.update(value => !value);
+  }
+
+
 
 
 }
