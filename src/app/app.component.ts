d4e04b9efe12ac526b9d427c0b1b3c2544c262b4
Denis <denisdenis009@gmail.com>
2024-11-30 02:07:32 +0200

    Split navigation into separate component, updated routing module to handle home, about, myfiles, and contact pages, reworked navigation, added responsiveness to navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HttpClient } from '@angular/common/http';
+import { NavigationComponent } from "./navigation/navigation.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/myfiles/myfiles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myfiles',
+  standalone: true,
+  imports: [],
+  templateUrl: './myfiles.component.html',
+  styleUrl: './myfiles.component.scss'
+})
+export class MyfilesComponent {
+
+}
